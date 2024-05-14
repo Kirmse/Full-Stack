211dd1e9ef50c219d0b04667e440d5521d6fee6e
Isaque Kirmse <kirmse2005@gmail.com>
2024-05-14 17:10:36 -0300

    Full Stack

---
+++ Codificação/Logica de programação/index.js
@@ -0,0 +1,7 @@
+// var estudanteNome = "Helena";
+// console.log(estudanteNome);
+
+// const EstudanteDoNome = "Heleina";
+// console.log(EstudanteDoNome);
+
+let listaEstudates = ["Helena" , "Enrico" , "Calebe" , "Flora"];
