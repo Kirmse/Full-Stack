8c58c237f0585e161db65710f2a9a3f1d7b7809e
Isaque Kirmse <kirmse2005@gmail.com>
2024-05-14 18:05:41 -0300

    Full Stack

--- Codificação/Logica de programação/index.js
+++ Codificação/Logica de programação/index.js
@@ -4,4 +4,36 @@
 // const EstudanteDoNome = "Heleina";
 // console.log(EstudanteDoNome);
 
+// for ----------------------------
+
+
+// let listaEstudates = ["Helena" , "Enrico" , "Calebe" , "Flora"];
+// let quantidadeDeEstudantes = listaEstudates.length;
+
+
+// for (let indice = 0; indice < quantidadeDeEstudantes; indice++) {
+//     const alunoCorrente = listaEstudates[indice];
+//     console.log(alunoCorrente);
+// }
+
+
 let listaEstudates = ["Helena" , "Enrico" , "Calebe" , "Flora"];
+let quantidadeDeEstudantes = listaEstudates.length;
+let inicio = 0;
+
+// do ------------------------ do \\
+
+do {
+    console.log(listaEstudates[inicio]);
+    inicio ++;
+    
+} while (inicio < quantidadeDeEstudantes);
+
+
+// while -------------------- while \\
+inicio = 0;
+
+while (inicio < quantidadeDeEstudantes) {
+    console.log(listaEstudates[inicio]);
+    inicio ++;
+}
